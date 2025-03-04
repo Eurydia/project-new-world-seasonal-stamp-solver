db4386de70bf40b4de6d5f2bef0ff517c1b0a047
Eurydia <crossdeltavi@gmail.com>
2025-03-04 23:30:00 +0100

    added basic feature to produce new state from existing state by changing an entry

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Solver.CardState;
 import Solver.RandomNumberGenerator;
 
 public class Main {
@@ -5,10 +6,12 @@ public class Main {
         System.out.println(1 << 16);
         System.out.println(Math.pow(2, 16));
         System.out.println(Integer.valueOf(0b0000000000000001));
-        var randomizer = new RandomNumberGenerator(0);
+        var randomizer = new RandomNumberGenerator(0
+        );
 
-        System.out.println(randomizer.random(4, 0, 17));
-
-
+        System.out.println(String.format("%" + 16 + "s", Integer.toBinaryString(12)).replace(' ', '0'));
+        var cardState = new CardState(60000, 3);
+        var moves = cardState.getValidMoves();
+        System.out.println(moves);
     }
 }
--- src/Solver/CardState.java
+++ src/Solver/CardState.java
@@ -1,11 +1,15 @@
 package Solver;
 
-public class CardState {
-    private static final int STATE_COUNT = 1 << 16;
-    private final int state;
-    private final byte freeMoveCount;
+import java.util.ArrayList;
+import java.util.List;
 
-    public CardState(int state, byte freeMoveCount) {
+public class CardState {
+    private static final int STATE_COUNT = 65536;
+    private static final int LAST_STATE = STATE_COUNT - 1;
+    private final int state;
+    private final int freeMoveCount;
+
+    public CardState(int state, int freeMoveCount) {
         if (freeMoveCount > 3 || freeMoveCount < 0) {
             throw new IllegalArgumentException();
         }
@@ -17,4 +21,39 @@ public class CardState {
         this.state = state;
         this.freeMoveCount = freeMoveCount;
     }
+
+    @Override
+    public String toString() {
+        String bitRepr = String.format("%16s", Integer.toBinaryString(this.state)).replace(' ', '0');
+        return String.format("state=%s", bitRepr);
+    }
+
+    public List<CardState> getValidMoves() {
+        if (freeMoveCount == 0) {
+            return List.of(this);
+        }
+
+        List<CardState> possibleNextMoves = new ArrayList<>();
+        char[] bits = String.format("%16s", Integer.toBinaryString(this.state)).replace(' ', '0').toCharArray();
+        for (int i = 0; i < 16; i++) {
+            if (bits[i] == '0') {
+                possibleNextMoves.add(this.stampAt(i / 4, i % 4));
+            }
+        }
+        return possibleNextMoves;
+    }
+
+    public CardState stampAt(int row, int col) {
+        int pos = (row * 4) + col;
+        char[] bits = String.format("%16s", Integer.toBinaryString(this.state)).replace(' ', '0').toCharArray();
+
+        if (bits[pos] == '1') {
+            return new CardState(this.state, freeMoveCount - 1);
+        }
+        bits[pos] = '1';
+
+        CardState nextState = new CardState(Integer.parseInt(new String(bits), 2), this.freeMoveCount - 1);
+        return nextState;
+    }
+
 }
